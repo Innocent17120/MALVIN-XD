0d00f8a23c41908118dd7c232a1747eab57b5809
Innocent17120 <Innocentpsizyongo17120@gmail.com>
2025-07-29 13:42:03 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~d29hmBia#VEfpbgfU_fNdMyaKEE7HvUcWRNETatxPlfqspODRxVI ",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
